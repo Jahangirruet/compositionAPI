9e0c2e52ef81f5e779605b958eb7e87dfcad93b7
Jahangir Alam <jahangir.2012@outlool.com>
2023-07-12 16:52:18 +0600

    CompositionAPI

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav><ul>
+       <li> <RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/LessionOne">LessionOne</RouterLink></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
